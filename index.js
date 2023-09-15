6f19cdcf7f5d0d6fa173e27a9af6a55d6b1cab55
sharifulislamshihan <sharifulislamshihan@gmail.com>
2023-09-16 03:59:35 +0600

    done

--- index.js
+++ index.js
@@ -14,49 +14,49 @@ const loadTubeVideosHandler = async() =>{
     });
 }
 // sorting
-const loadSortByView = async() =>{
-    const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/1000`);
-    const data = await res.json();
-    const cardContainer = document.getElementById('card-container');
-    sortByView = data.data;
-    cardContainer.innerHTML = "";
-    sortByView?.forEach((data) => {
-        sortByView?.sort((ob1, ob2) => parseFloat(ob2?.others?.views) - parseFloat(ob1?.others?.views));
-        console.log(data);
-        const div = document.createElement('div')
-        div.innerHTML = `
-                <div class="card bg-base-100">
-                <figure><img class="w-96 h-44 rounded-xl" src="${data?.thumbnail}" alt="" /></figure>
-                <div class=" relative bottom-10 right-3 flex justify-end ml-40 bg-slate-800 text-white text-sm">
-                <h3 class="p-1 pr-3"><span>3</span>hrs <span>54</span> min ago</h3>
-                </div>
-                <div class="card-body -ml-8">
-                    <div class="flex gap-5">
-                        <img class="w-11 h-11 rounded-full" 
-                        src="${data?.authors[0]?.profile_picture}" alt="">
-                        <h2 class="text-lg font-bold"> ${data?.title}
-                        </h2>
-                    </div>
+// const loadSortByView = async() =>{
+//     const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/1000`);
+//     const data = await res.json();
+//     const cardContainer = document.getElementById('card-container');
+//     sortByView = data.data;
+//     cardContainer.innerHTML = "";
+//     sortByView?.forEach((data) => {
+//         sortByView?.sort((ob1, ob2) => parseFloat(ob2?.others?.views) - parseFloat(ob1?.others?.views));
+//         console.log(data);
+//         const div = document.createElement('div')
+//         div.innerHTML = `
+//                 <div class="card bg-base-100">
+//                 <figure><img class="w-96 h-44 rounded-xl" src="${data?.thumbnail}" alt="" /></figure>
+//                 <div class=" relative bottom-10 right-3 flex justify-end ml-40 bg-slate-800 text-white text-sm">
+//                 <h3 class="p-1 pr-3"><span>3</span>hrs <span>54</span> min ago</h3>
+//                 </div>
+//                 <div class="card-body -ml-8">
+//                     <div class="flex gap-5">
+//                         <img class="w-11 h-11 rounded-full" 
+//                         src="${data?.authors[0]?.profile_picture}" alt="">
+//                         <h2 class="text-lg font-bold"> ${data?.title}
+//                         </h2>
+//                     </div>
     
-                    <!-- Name -->
-                    <div class="ml-16 -mt-3">
-                        <h2 class="card-title text-[#111111B3] text-sm">
-                            ${data?.authors[0]?.profile_name}
-                            <div class="bedge">
-                                <img class="hidden" src="blueTick.png" alt="">
-                            </div>
-                        </h2>
-                    </div>
-                    <h2 class="text-[#111111B3] ml-16 text-sm"><span>${data?.others?.views}</span> views</h2>
-                </div>
-            </div>
-        `
+//                     <!-- Name -->
+//                     <div class="ml-16 -mt-3">
+//                         <h2 class="card-title text-[#111111B3] text-sm">
+//                             ${data?.authors[0]?.profile_name}
+//                             <div class="bedge">
+//                                 <img class="hidden" src="blueTick.png" alt="">
+//                             </div>
+//                         </h2>
+//                     </div>
+//                     <h2 class="text-[#111111B3] ml-16 text-sm"><span>${data?.others?.views}</span> views</h2>
+//                 </div>
+//             </div>
+//         `
         
     
-        cardContainer.appendChild(div); 
+//         cardContainer.appendChild(div); 
 
-    });
-}
+//     });
+// }
 
 const loadVideos = async(categoryId) =>{
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
