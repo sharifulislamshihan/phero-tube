7d07e18c47997e8ea7fa92a95c6447662dd20714
sharifulislamshihan <sharifulislamshihan@gmail.com>
2023-09-16 03:42:32 +0600

    final

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const loadTubeVideosHandler = async() =>{
     const data = await res.json();
     const tabContainer = document.getElementById('tab-container');
     data.data.forEach((category) => {
-        // console.log(category.category);
         const div = document.createElement('div');
         div.innerHTML = `
         <a onclick= "loadVideos('${category?.category_id}')" class="tab rounded-lg bg-slate-300 text-xl text-black ">${category?.category}</a> 
@@ -13,17 +12,15 @@ const loadTubeVideosHandler = async() =>{
         tabContainer.appendChild(div);
 
     });
-    // console.log(data.data);
-    // console.log(data.data[1].category);
 }
 // sorting
 const loadSortByView = async() =>{
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/1000`);
     const data = await res.json();
-    const cardContainer1 = document.getElementById('card-container1');
+    const cardContainer = document.getElementById('card-container');
     sortByView = data.data;
+    cardContainer.innerHTML = "";
     sortByView?.forEach((data) => {
-        //console.log(data.data[0].others.views);
         sortByView?.sort((ob1, ob2) => parseFloat(ob2?.others?.views) - parseFloat(ob1?.others?.views));
         console.log(data);
         const div = document.createElement('div')
@@ -56,34 +53,23 @@ const loadSortByView = async() =>{
         `
         
     
-        cardContainer1.appendChild(div); 
+        cardContainer.appendChild(div); 
+
     });
 }
 
 const loadVideos = async(categoryId) =>{
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
-    //console.log(categoryId);
     const data = await res.json();
     const cardContainer1 = document.getElementById('card-container1');
     const cardContainer2 = document.getElementById('card-container2');
     cardContainer1.innerHTML = "";
     cardContainer2.innerHTML = "";
     const len = data.data.length;
-    //console.log(len);
     if(len >= 1){
         cardContainer1.classList.remove('hidden');
         cardContainer2.classList.add('hidden');
         data.data?.forEach((video) => {
-            //const time = video?.others?.posted_date
-            // if(time <3600){
-            //     const min = parseInt(time/60);
-            // }
-            // else{
-            //     const hour = parseInt(time/3600);
-            //     const leftTime = time - (hour*3600);
-            //     const min = parseInt(leftTime/60);
-            // }
-            //console.log(time);
             const div = document.createElement('div')
         div.innerHTML = `
                 <div class="card bg-base-100">
@@ -131,9 +117,5 @@ const loadVideos = async(categoryId) =>{
         cardContainer2.appendChild(div); 
     }
 }
-// const sortedByView = () =>{
-
-// }
-// }
-// loadVideos();
 loadTubeVideosHandler();
+
